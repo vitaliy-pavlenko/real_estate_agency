070dd5fdae410d7ffd492c51cb496532b5b0504b
Vitaliy Pavlenko <vpavlenko@iponweb.net>
2021-08-01 13:07:52 +0300

    fix duplicated flat-owner m2m relations

--- property/admin.py
+++ property/admin.py
@@ -4,17 +4,17 @@ from .models import Complaint, Flat, Owner
 
 
 class OwnerInline(admin.TabularInline):
-    model = Flat.owner.through
+    model = Owner.flats.through
     raw_id_fields = ['owner']
 
 
 class FlatAdmin(admin.ModelAdmin):
-    search_fields = ['town', 'address', 'owner']
+    search_fields = ['town', 'address']
     readonly_fields = ['created_at']
     list_display = ['address', 'price', 'new_building', 'construction_year', 'town']
     list_editable = ['new_building']
     list_filter = ['new_building', 'rooms_number', 'floor']
-    raw_id_fields = ['liked_by', 'owner']
+    raw_id_fields = ['liked_by']
     inlines = [OwnerInline]
 
 
